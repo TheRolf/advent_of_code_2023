0a7614b2d270ee703641f580a69ce6e877b82372
TheRolf <roli.vincze@gmail.com>
2023-12-15 17:16:18 +0100

    Refactor p1 of day 14

--- src/day_14.rs
+++ src/day_14.rs
@@ -2,43 +2,63 @@
 
 use advent_of_code_2023::*;
 
-pub fn tilt(mut input: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    for i in 1..input.len() {
-        for j in 0..input[0].len() {
-            if input[i][j] == 'O' {
-                for i_new in (0..i).rev() {
-                    if input[i_new][j] != '.' {
-                        input[i][j] = '.';
-                        input[i_new + 1][j] = 'Ö';
-                        break;
-                    }
-                }
-                if input[i][j] == 'O' {
-                    input[0][j] = 'Ö';
-                    input[i][j] = '.';
-                }
-            }
-        }
-    }
-    input
+struct Platform {
+    cells: Vec<Vec<char>>,
+    height: usize,
+    width: usize,
 }
 
-pub fn total_load(input: &Vec<Vec<char>>) -> usize {
-    let mut total: usize = 0;
-    for (i, row) in input.iter().enumerate() {
-        for c in row {
-            if *c == 'O' || *c == 'Ö' {
-                total += input.len() - i;
+impl Platform {
+    pub fn new(input: Vec<Vec<char>>) -> Self {
+        let height: usize = input.len();
+        let width: usize = input[0].len();
+        Self {
+            cells: input,
+            height,
+            width,
+        }
+    }
+
+    pub fn tilt(&mut self) {
+        for i in 1..self.height {
+            for j in 0..self.width {
+                if self.cells[i][j] == 'O' {
+                    let mut moved: bool = false;
+                    for i_new in (0..i).rev() {
+                        if self.cells[i_new][j] != '.' {
+                            self.cells[i][j] = '.';
+                            self.cells[i_new + 1][j] = 'O';
+                            moved = true;
+                            break;
+                        }
+                    }
+                    if moved == false {
+                        self.cells[0][j] = 'O';
+                        self.cells[i][j] = '.';
+                    }
+                }
             }
         }
     }
-    total
+
+    pub fn total_load(&self) -> usize {
+        let mut total: usize = 0;
+        for (i, row) in self.cells.iter().enumerate() {
+            for c in row {
+                if *c == 'O' {
+                    total += self.height - i;
+                }
+            }
+        }
+        total
+    }
 }
 
 pub fn main() {
-    let mut input: Vec<Vec<char>> = puzzle_input_asarray(14, false);
-    print_2d_array(&input);
-    input = tilt(input);
-    print_2d_array(&input);
-    println!("{}", total_load(&input));
+    let mut input: Vec<Vec<char>> = puzzle_input_asarray(14, true);
+    let mut platform: Platform = Platform::new(input);
+    print_2d_array(&platform.cells);
+    platform.tilt();
+    print_2d_array(&platform.cells);
+    println!("{}", platform.total_load());
 }
