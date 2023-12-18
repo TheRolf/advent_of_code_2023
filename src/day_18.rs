7734e419d869325a8dcdc31d83e7ec2d776bb4dc
TheRolf <roli.vincze@gmail.com>
2023-12-19 00:12:27 +0100

    Day 18 part1

--- src/day_18.rs
+++ src/day_18.rs
@@ -1,15 +1,15 @@
 #![allow(dead_code, unused_variables, unused_mut)]
 
-use std::{str::FromStr, collections::HashMap};
-use itertools::Itertools; // 0.8.2
 use advent_of_code_2023::*;
+use itertools::Itertools; // 0.8.2
+use std::{collections::HashMap, str::FromStr};
 
 #[derive(PartialEq)]
 enum Direction {
     Up,
+    Right,
     Down,
     Left,
-    Right
 }
 
 impl FromStr for Direction {
@@ -27,10 +27,10 @@ impl FromStr for Direction {
     }
 }
 
-struct Instruction{
+struct Instruction {
     direction: Direction,
     steps: usize,
-    colour: String, 
+    colour: String,
 }
 
 impl Instruction {
@@ -39,7 +39,11 @@ impl Instruction {
         let direction: Direction = split_text[0].parse::<Direction>().unwrap();
         let steps: usize = split_text[1].parse::<usize>().unwrap();
         let colour: String = split_text[2][1..8].to_string();
-        Self { direction, steps, colour}
+        Self {
+            direction,
+            steps,
+            colour,
+        }
     }
 }
 
@@ -54,27 +58,48 @@ struct Terrain {
 }
 
 impl Terrain {
-    pub fn new(input: Vec<String>) -> Self{
+    pub fn new(input: Vec<String>) -> Self {
         let mut instructions: Vec<Instruction> = Vec::new();
         for line in input {
             let instruction = Instruction::new(line);
             instructions.push(instruction);
         }
-        Self {instructions, ..Default::default()}
+        Self {
+            instructions,
+            ..Default::default()
+        }
     }
 
-    pub fn dig(&mut self){
+    pub fn print(&self) {
+        for (i, j_values) in self.trench.iter().sorted() {
+            let mut j: i32 = 0;
+            let mut found: usize = 0;
+            while found < j_values.len() {
+                if j_values.contains(&j) {
+                    print!("#");
+                    found += 1;
+                } else {
+                    print!(".");
+                }
+                j += 1;
+            }
+            println!(" {}", i);
+        }
+        println!();
+    }
+
+    pub fn dig(&mut self) {
         for instr in &self.instructions {
             match instr.direction {
-                Direction::Down | Direction::Up  => {
+                Direction::Down | Direction::Up => {
                     for i in 0..instr.steps {
-                        self.pos_i += if instr.direction == Direction::Down {1} else {-1};
+                        self.pos_i += if instr.direction == Direction::Down { 1 } else { -1 };
                         self.trench.entry(self.pos_i).or_insert(Vec::new()).push(self.pos_j);
                     }
                 }
                 Direction::Left | Direction::Right => {
                     for j in 0..instr.steps {
-                        self.pos_j += if instr.direction == Direction::Right {1} else {-1};
+                        self.pos_j += if instr.direction == Direction::Right { 1 } else { -1 };
                         self.trench.entry(self.pos_i).or_insert(Vec::new()).push(self.pos_j);
                     }
                 }
@@ -85,20 +110,124 @@ impl Terrain {
         }
     }
 
-    pub fn area(&self) -> usize {
-        let mut total_area = 0;
+    // trial 1, incorrect
+    pub fn area_with_print(&self) -> usize {
+        let mut total_area: usize = 0;
         for (i, j_values) in self.trench.iter().sorted() {
-            
+            let mut j: i32 = 0;
+            let mut found: usize = 0;
+            while found < j_values.len() {
+                if j_values.contains(&j) {
+                    print!("#");
+                    found += 1;
+                } else {
+                    print!(".");
+                }
+                j += 1;
+            }
+
+            let mut prev_j: i32 = j_values[0];
+            let mut inside: bool = true;
+            let mut prev_jump: bool = false;
+            print!(" ({})", j_values.len());
+            total_area += j_values.len();
+            for j in &j_values[1..] {
+                if *j - prev_j > 1 {
+                    if prev_jump {
+                        inside = !inside;
+                    }
+                    if inside {
+                        print!(" ({})", (*j - prev_j - 1));
+                        total_area += (*j - prev_j - 1) as usize;
+                        prev_jump = true;
+                    }
+                } else {
+                    prev_jump = false;
+                }
+                prev_j = *j;
+            }
+            println!(" {}", total_area);
         }
         total_area
+    }
+
+    // trial 2, stack overflow (all variations)
+    pub fn fill_recurse(&mut self, i: i32, j: i32, rev: bool) {
+        println!("{} {}", i, j);
+        let choices = if rev {
+            vec![(0, 1), (0, -1)]
+        } else {
+            vec![(1, 0), (-1, 0)]
+        };
+        for (d_i, d_j) in choices {
+            let mut m: i32 = 1;
+            let mut new_i = i + m * d_i;
+            let mut new_j = j + m * d_j;
+            while self.trench.contains_key(&new_i) && !self.trench.get(&new_i).unwrap().contains(&new_j) {
+                self.trench.entry(new_i).or_insert(Vec::new()).push(new_j);
+                self.fill_recurse(new_i, new_j, !rev);
+                m += 1;
+                new_i = i + m * d_i;
+                new_j = j + m * d_j;
+            }
+        }
+    }
+
+    // trial 3, incorrect
+    pub fn fill(&mut self, i: i32, j: i32) {
+        let preference = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
+        let mut i = i;
+        let mut j = j;
+        let mut d_i;
+        let mut d_j;
+        let mut arrow: i32 = 1;
+        'outer: loop {
+            self.trench.entry(i).or_insert(Vec::new()).push(j);
+            for try_move in (arrow - 1)..(arrow + 3) {
+                (d_i, d_j) = preference[((try_move + 4) % 4) as usize];
+                if !self.trench.get(&(i + d_i)).unwrap().contains(&(j + d_j)) {
+                    arrow = try_move;
+                    i += d_i;
+                    j += d_j;
+                    continue 'outer;
+                }
+            }
+            break;
+        }
+    }
+
+    // trial 4: correct!
+    pub fn fill_recurse_iter(&mut self, i_start: i32, j_start: i32) {
+        let mut queue: Vec<(i32, i32)> = Vec::new();
+        queue.push((i_start, j_start));
+        let mut i: i32;
+        let mut j: i32;
+        while queue.len() > 0 {
+            (i, j) = queue.pop().unwrap();
+            // println!("{} {}, {}", i, j, queue.len());
+            self.trench.entry(i).or_insert(Vec::new()).push(j);
+            for (d_i, d_j) in vec![(0, 1), (0, -1), (1, 0), (-1, 0)] {
+                let new_i = i + d_i;
+                let new_j = j + d_j;
+                if self.trench.contains_key(&new_i)
+                    && !self.trench.get(&new_i).unwrap().contains(&new_j)
+                    && !queue.contains(&(new_i, new_j))
+                {
+                    queue.push((new_i, new_j));
+                }
+            }
+        }
+    }
+
+    pub fn total(&self) -> usize {
+        self.trench.values().map(|x| x.len()).sum()
     }
 }
 
 pub fn main() {
-    let input = puzzle_input_aslines(18, true);
+    let input = puzzle_input_aslines(18, false);
     let mut terrain = Terrain::new(input);
     terrain.dig();
-    for (i, row) in terrain.trench.iter() {
-        println!("{}: {:?}", i, row);
-    }
+    terrain.fill_recurse_iter(-84, 11);
+    println!("Area: {}", terrain.total());
 }
