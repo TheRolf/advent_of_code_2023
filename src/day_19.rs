32ebaa5511fc1b083e915990f41839396f0fb389
TheRolf <roli.vincze@gmail.com>
2023-12-20 02:54:23 +0100

    Day19 p1 done, p2 brute-force

---
+++ src/day_19.rs
@@ -0,0 +1,155 @@
+#![allow(dead_code, unused_variables, unused_mut)]
+
+use std::collections::HashMap;
+
+use advent_of_code_2023::*;
+
+#[derive(Debug)]
+struct Part {
+    rating: HashMap<char, usize>,
+}
+
+impl Part {
+    pub fn new(text: String) -> Self {
+        let mut rating: HashMap<char, usize> = HashMap::new();
+        let entries: Vec<&str> = text[1..text.len() - 1].split(",").collect();
+        for entry in entries {
+            let var: char = entry.chars().nth(0).unwrap();
+            let value: usize = entry[2..].parse::<usize>().unwrap();
+            rating.insert(var, value);
+        }
+        Self { rating }
+    }
+    pub fn value(&self) -> usize {
+        self.rating.values().sum()
+    }
+}
+
+#[derive(Debug)]
+struct Rule {
+    var: char,
+    cond: char,
+    value: usize,
+    step: String,
+}
+
+#[derive(Debug)]
+struct Workflow {
+    rules: Vec<Rule>,
+    or_else: String,
+}
+
+impl Workflow {
+    pub fn new(text: String) -> Self {
+        let mut rules: Vec<Rule> = Vec::new();
+        let mut or_else: String = "".to_string();
+        let split_text: Vec<&str> = text.split(",").collect();
+        for cmd in split_text {
+            if cmd.contains(':') {
+                let var = cmd.chars().nth(0).unwrap();
+                let cond = cmd.chars().nth(1).unwrap();
+                let second_half: Vec<&str> = cmd[2..].split(":").collect();
+                let value = second_half[0].parse::<usize>().unwrap();
+                let step = second_half[1].to_string();
+                rules.push(Rule { var, cond, value, step });
+            } else {
+                or_else = cmd.to_string();
+            }
+        }
+        Self { rules, or_else }
+    }
+
+    pub fn process(&self, part: &Part) -> &String {
+        for rule in &self.rules {
+            match rule.cond {
+                '<' => {
+                    if part.rating.get(&rule.var).unwrap() < &rule.value {
+                        return &rule.step;
+                    }
+                }
+                '>' => {
+                    if part.rating.get(&rule.var).unwrap() > &rule.value {
+                        return &rule.step;
+                    }
+                }
+                _ => {}
+            }
+        }
+        &self.or_else
+    }
+}
+
+struct System {
+    workflows: HashMap<String, Workflow>,
+}
+
+impl System {
+    pub fn new() -> Self {
+        Self {
+            workflows: HashMap::new(),
+        }
+    }
+
+    pub fn add(&mut self, text: String) {
+        let split_text: Vec<&str> = text.split("{").collect();
+        let workflow: Workflow = Workflow::new(split_text[1][..(split_text[1].len() - 1)].to_string());
+        self.workflows.insert(split_text[0].to_string(), workflow);
+    }
+
+    pub fn accepts(&self, part: &Part) -> bool {
+        let mut label: &String = &"in".to_string();
+        while label != &"A".to_string() && label != &"R".to_string() {
+            label = self.workflows.get(label).unwrap().process(&part);
+        }
+        label.as_str() == "A"
+    }
+
+    pub fn process(&self, part: &Part) -> usize {
+        if self.accepts(part) {
+            return part.value();
+        }
+        0
+    }
+}
+
+pub fn main() {
+    let input = puzzle_input_aslines(19, false);
+    let mut system: System = System::new();
+    let mut parts: Vec<Part> = Vec::new();
+    let mut rules = true;
+    for line in input {
+        if line.is_empty() {
+            rules = false;
+        } else if rules {
+            system.add(line)
+        } else {
+            parts.push(Part::new(line));
+        }
+    }
+
+    let mut sum_a: usize = 0;
+    for part in &parts {
+        sum_a += &system.process(part);
+    }
+    println!("{}", sum_a);
+
+    let mut sum_b: usize = 0;
+    for x in 1..4001 {
+        for m in 1..4001 {
+        println!("{} {} {}", x, m, sum_b);
+            for a in 1..4001 {
+                for s in 1..4001 {
+                    let mut rating: HashMap<char, usize> = HashMap::new();
+                    rating.insert('x', x);
+                    rating.insert('m', m);
+                    rating.insert('a', a);
+                    rating.insert('s', s);
+                    let part: Part = Part { rating };
+                    if system.accepts(&part) {
+                        sum_b += 1;
+                    }
+                }
+            }
+        }
+    }
+}
