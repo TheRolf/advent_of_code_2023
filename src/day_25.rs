be885788c10f4e49077dda83eaae0eeb2f8f2c91
Roland <roli.vincze@gmail.com>
2023-12-25 13:53:06 +0100

    Day25 own Graph class

--- src/day_25.rs
+++ src/day_25.rs
@@ -3,29 +3,36 @@
 use std::collections::{HashMap, HashSet};
 
 use advent_of_code_2023::*;
-use graph::prelude::{CsrLayout, GraphBuilder, UndirectedCsrGraph};
+
+#[derive(Debug)]
+struct Graph {
+    nodes: HashSet<String>,
+    adjacent_nodes: HashMap<String, HashSet<String>>,
+}
+
+impl Graph {
+    pub fn new(input: &Vec<String>) -> Self {
+        let mut nodes = HashSet::new();
+        let mut adjacent_nodes = HashMap::new();
+
+        for line in input {
+            let split_line: Vec<&str> = line.split(": ").collect();
+            let root = split_line[0].to_string();
+            let adjacents: Vec<&str> = split_line[1].split(" ").collect();
+            nodes.insert(root.clone());
+            for conn in adjacents {
+                nodes.insert(conn.to_string());
+                adjacent_nodes.entry(root.to_string()).or_insert(HashSet::new()).insert(conn.to_string());
+                adjacent_nodes.entry(conn.to_string()).or_insert(HashSet::new()).insert(root.to_string());
+            }
+        }
+
+        Self { nodes, adjacent_nodes }
+    }
+}
 
 pub fn main() {
     let input = puzzle_input_aslines(25, true);
-    // let mut builder: GraphBuilder<String> = GraphBuilder::edges(self, edges)
-    let mut nodes_str: HashSet<String> = HashSet::new();
-    let mut edges_str: Vec<(String, String)> = Vec::new();
-    for line in input {
-        let split_text: Vec<&str> = line.split(": ").collect();
-        let root = split_text[0];
-        let connections: Vec<&str> = split_text[1].split(" ").collect();
-        nodes_str.insert(root.to_string());
-        for conn in connections {
-            nodes_str.insert(conn.to_string());
-            edges_str.push((root.to_string(), conn.to_string()));
-        }
-    }
-    let mut nodes = Vec::from_iter(nodes_str.clone());
-    let mut edges: Vec<(usize, usize)> = Vec::new();
-    for (edge_a, edge_b) in edges_str {
-        let edge_i = nodes_str.iter().position(|e| *e == edge_a).unwrap();
-        let edge_j = nodes_str.iter().position(|e| *e == edge_b).unwrap();
-        edges.push((edge_i, edge_j));
-    }
-    let g: UndirectedCsrGraph<usize> = GraphBuilder::new().csr_layout(CsrLayout::Sorted).edges(edges).build();
+    let mut graph: Graph = Graph::new(&input);
+    println!("{:#?}", graph);
 }
